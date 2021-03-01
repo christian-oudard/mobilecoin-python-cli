a367f35466dc67abd0fc60342ecd3bab2e7b287d
Christian Oudard <christian.oudard@gmail.com>
2021-03-01 14:16:59 -0800

    Add executable script to the setup file.

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ from distutils.core import setup
 setup(
     name='mobilecoin-cli',
     version='0.1',
-    description='',
+    description='Command line client for MobileCoin.',
     author='Christian Oudard',
-    author_email='christian.oudard@gmail.com',
+    author_email='christian@christianoudard.com',
     url='https://github.com/christian-oudard/mobilecoin-python-cli',
     packages=['mobilecoin'],
+    scripts=['bin/mobcli'],
 )
